89bd65f89d91cd9ffd49011b235c27bcf508b109
Kevin Baer <149030800+kevbaer@users.noreply.github.com>
2026-06-09 21:36:49 -0700

    Start work

---
+++ 01_Data_Gather.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(anyflights)
+library(ggview)
+
+# for(y in 2018:2025){
+#   get_flights("LAX", year = y, dir = "99_data")
+# }
+
+full_flight_df <- tibble()
+
+for (i in list.files("99_data", full.names = TRUE)) {
+  load(i)
+  full_flight_df <- bind_rows(full_flight_df, flights)
+}
+
+rm(flights)
---
+++ 02_Exposition.R
@@ -0,0 +1,36 @@
+set_theme(theme_bw(base_size = 16, base_family = "Barlow"))
+
+flights_per_day <- full_flight_df |>
+  summarize(flights_per_day = n(), .by = c(year, month, day)) |>
+  mutate(ymd = make_date(year = year, month = month, day = day), .before = 1) |>
+  select(-c(year, month, day))
+
+plot_1 <- flights_per_day |>
+  ggplot() +
+  aes(x = ymd, y = flights_per_day) +
+  geom_line(color = "#5c4cbf") +
+  geom_vline(
+    xintercept = ymd("2021-07-01"),
+    linetype = "dashed",
+    linewidth = .8,
+    color = "#b3114b"
+  ) +
+  geom_vline(
+    xintercept = ymd("2025-07-01"),
+    linetype = "dashed",
+    linewidth = .8,
+    color = "#b3114b"
+  ) +
+  labs(
+    title = "Effect of Covid on Flights Departing LAX",
+    y = "Flights per Day",
+    x = NULL
+  ) +
+  scale_x_date(expand = expansion(mult = c(.02, .02))) +
+  canvas(width = 8, height = 6)
+
+train_set <- flights_per_day |>
+  filter(ymd >= ymd("2021-07-01") & ymd < ymd("2025-07-01"))
+
+test_set <- flights_per_day |>
+  filter(ymd >= ymd("2025-07-01"))
